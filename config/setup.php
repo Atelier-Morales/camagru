ea8cd1f57d0c655a7eb3bf201b3a1d435018d2dd
Atelier-Morales <fmorales@student.42.fr>
2016-08-09 11:23:18 +0200

    finished the database creation diagram

--- config/setup.php
+++ config/setup.php
@@ -1,24 +1,76 @@
 
+<?php
+require("./database.php");
 
-likes :
+try {
+    $user_table = $db->prepare("CREATE TABLE `test`.`user`
+        (
+            `id` INT NOT NULL AUTO_INCREMENT,
+            `username` VARCHAR(64) NOT NULL,
+            `email` VARCHAR(140) NOT NULL,
+            `password` CHAR(64) NOT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE (`username`),
+            UNIQUE (`password`)
+        )
+        ENGINE = InnoDB");
+    $user_table->execute();
 
-CREATE TABLE Likes(
-id   INT              NOT NULL,
-username VARCHAR (20)     NOT NULL,
-picture_id  INT              NOT NULL,
-PRIMARY KEY (id)
-);
+    try {
+        $pictures_table = $db->prepare("CREATE TABLE `test`.`pictures`
+        (
+            `id` INT NOT NULL AUTO_INCREMENT,
+            `src` LONGBLOB NOT NULL,
+            `title` VARCHAR(64) NOT NULL ,
+            `date` VARCHAR(64) NOT NULL,
+            `user_id` INT NOT NULL,
+            PRIMARY KEY (`id`)
+        )
+        ENGINE = InnoDB");
+        $pictures_table->execute();
 
-table USER :
+        try {
+            $likes_table = $db->prepare("CREATE TABLE `test`.`Likes`
+            (
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `username` VARCHAR(64) NOT NULL,
+                `picture_id`  INT NOT NULL,
+                PRIMARY KEY (`id`)
+            )
+            ENGINE = InnoDB");
+            $likes_table->execute();
 
-CREATE TABLE `test`.`user`
-(
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `username` VARCHAR(64) NULL DEFAULT NULL ,
-    `email` VARCHAR(140) NOT NULL ,
-    `password` CHAR(64) NOT NULL ,
-    PRIMARY KEY (`id`),
-    UNIQUE (`username`),
-    UNIQUE (`password`)
-)
-ENGINE = InnoDB;
+            try {
+                $comment_table = $db->prepare("CREATE TABLE `test`.`Comments`
+                (
+                    `id` INT NOT NULL AUTO_INCREMENT,
+                    `username` VARCHAR(64) NOT NULL,
+                    `picture_id`  INT NOT NULL,
+                    `comment` VARCHAR(64) NOT NULL,
+                    `date` VARCHAR(64) NOT NULL,
+                    PRIMARY KEY (`id`)
+                )
+                ENGINE = InnoDB");
+                $comment_table->execute();
+
+                echo "database scheme creation successful !";
+            }
+            catch (PDOException $e) {
+                echo "error : could not create the table likes";
+            }
+        }
+        catch (PDOException $e) {
+            echo "error : could not create the table likes";
+        }
+    }
+    catch (PDOException $e) {
+        echo $e->getMessage();
+        echo "error : could not create the Table pictures";
+    }
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+    echo "error : could not create the tables in the db";
+}
+
+?>
